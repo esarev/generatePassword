3e507ce6d8bf9859b006ba53c6e4985d59c20da8
Alex <esarev@inbox.ru>
2021-11-17 16:54:49 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const info = document.querySelector('.info'),
+      passfield = document.querySelector('.input');
+let password = '';
+
+function generatePassword() {
+  password = '';
+  let length = 8;
+  let chars = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()`;
+  let n = chars.length;
+  for (let i = 0; i < length; i++) {
+    password += chars.charAt(Math.floor(Math.random() * n));
+  }
+  passfield.value = password;
+}
+
+function copytext() {
+  navigator.clipboard.writeText(password);
+  info.style.opacity = '1';
+  setTimeout(function() {
+    info.style.opacity = '0';
+  }, 1500);
+}
